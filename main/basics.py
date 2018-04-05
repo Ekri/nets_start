489fb474e815d30279202bb5a0e792cfee3bb5f8
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-04-05 06:03:55 +0200

    init

---
+++ main/basics.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+array = np.array(list(zip(iris.data, iris.target)))[0:10]
+# np.column_stack((iris.data, iris.target))[0:10]
+""" Good alternative when do not want to use python functions """
+
+print(array)
+# print(iris.DESCR)
+print(iris.target_names)

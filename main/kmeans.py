adf8af72673d887ee08dcfd8ed487e02ae45f0d0
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-04-06 06:04:56 +0200

    playing

--- main/kmeans.py
+++ main/kmeans.py
@@ -11,6 +11,7 @@ km.fit(X)
 predictions = km.predict(iris.data)
 
 colors = cycle('rgb')
+markers = cycle('^+o')
 labels = ["Cluster 1", "Cluster 2", "Cluster 3"]
 targets = range(len(labels))
 
@@ -18,4 +19,19 @@ feature_index = range(len(iris.feature_names))
 feature_names = iris.feature_names
 combs = combinations(feature_index, 2)
 
+f, axarr = pl.subplots(3, 2)
+axarr_flat = axarr.flat
 
+# for comb, axflat in zip(combs, axarr_flat):
+#     for target, color, label, marker in zip(targets, colors, labels, markers):
+#         feature_index_x = comb[0]
+#         feature_index_y = comb[1]
+#         axflat.scatter(iris.data[predictions == target, feature_index_x],
+#                        iris.data[predictions == target, feature_index_y], c=color, label=label, marker=marker)
+#         axflat.set_xlabel(feature_names[feature_index_x])
+#         axflat.set_ylabel(feature_names[feature_index_y])
+#
+# f.tight_layout()
+# pl.show()
+
+# TODO learn about visualisation in matplotlib and python is not installed as a framework error
